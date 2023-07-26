4411d03a6a0543cad77886d6bb7950784f3e0eda
praveenreddy33333 <136301124+praveenreddy33333@users.noreply.github.com>
2023-07-26 18:28:17 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
 
   tags = {
